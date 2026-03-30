1ebf7e5021ee243eba2fadfc69be52db11827363
byhabibi <by.habibi18@gmail.com>
2026-03-30 08:34:45 +0700

    delete addons

--- custom_addons/plc_monitoring/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import plc_data
--- custom_addons/plc_monitoring/models/plc_data.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PlcData(models.Model):
-    _name = 'plc.data'
-    _description = 'PLC Data'
-
-    machine_name = fields.Char(string="Machine")
-    counter = fields.Integer(string="Counter")
-    timestamp = fields.Datetime(string="Time")
